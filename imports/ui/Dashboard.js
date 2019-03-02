c04c762418049fa9d7e0235436a0af97d255d31e
Alex Rabideau <rabidale@outlook.com>
2019-03-02 15:41:51 -0500

    add meteor methods and basic react components

--- imports/ui/Dashboard.js
+++ imports/ui/Dashboard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import PrivateHeader from './PrivateHeader';
+import NoteList from './NoteList';
 
 export default () => {
   return (
     <div>
       <PrivateHeader title="Meteor Project Boilerplate"/>
       <div className="page-content">
-        Dashboard page content.
+        <NoteList/>
       </div>
     </div>
   );
---
+++ imports/ui/NoteListItem.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import expect from 'expect';
+import { Meteor } from 'meteor/meteor';
+import { mount } from 'enzyme';
+
+import NoteListItem from './NoteListItem';
+
+if (Meteor.isClient) {
+  describe('NoteListItem', function () {
+
+    it('should render title and timestamp', function () {
+      const title = 'My title here';
+      const updatedAt = 1551557354738;
+      const wrapper = mount( <NoteListItem note={{ title, updatedAt }}/> );
+
+      expect(wrapper.find('h5').text()).toBe(title);
+      expect(wrapper.find('p').text()).toBe('3/02/19');
+    });
+
+    it('should set default if no title set', function () {
+      const title = '';
+      const updatedAt = 1551557354738;
+      const wrapper = mount( <NoteListItem note={{ title, updatedAt }}/> );
+
+      expect(wrapper.find('h5').text()).toBe('Untitled note');
+    });
+  });
+}
