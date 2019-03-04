77a79b60d5e9d7f4e5aed8e44777e212005e65f6
Alex Rabideau <rabidale@outlook.com>
2019-03-03 23:32:33 -0500

    setup page content layout

--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -48,7 +48,7 @@ export class Editor extends React.Component {
 
     if (this.props.note) {
       return (
-        <div>
+        <div className="editor">
           <input
             value={this.state.title}
             placeholder="Untitled Note"
@@ -62,7 +62,9 @@ export class Editor extends React.Component {
       );
     } else {
       return (
-        <p>{this.props.selectedNoteId ? 'Note not found.' : 'Pick or create a note to get started.'}</p>
+        <div className="editor">
+          <p>{this.props.selectedNoteId ? 'Note not found.' : 'Pick or create a note to get started.'}</p>
+        </div>
       );
     }
   }
